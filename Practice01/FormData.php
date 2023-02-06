eb695d1fef07eb32a27f98a986afd12b083f36d0
Shafwan Ahmed Dehan <88941317+ShafwanAhmedDehan@users.noreply.github.com>
2023-02-06 20:46:35 +0600

    Add files via upload

---
+++ Practice01/FormData.php
@@ -0,0 +1,16 @@
+<?php 
+	echo "First Name : ";
+	echo$_POST ['fname'];
+	echo "<br>Last Name : ";
+	echo$_POST ['lname'];
+	echo "<br>Email : ";
+	echo$_POST ['email'];
+	echo "<br>Gender : ";
+	echo$_POST ['gender'];
+	echo "<br>Age Range : ";
+	echo$_POST ['agerange'];
+	echo "<br>Own Vehicle : ";
+	echo$_POST ['vehicle'];
+	echo "<br>Birthday : ";
+	echo$_POST ['birthday'];
+?>
